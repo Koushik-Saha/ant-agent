19d1281f71c2b3c3bd1d290061d96fbeb7d69599
Koushik Saha <sahakoushik@moveon.work>
2021-06-27 07:55:38 +0600

    add router

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+	Switch, Route
+} from 'react-router-dom'
+// import AuthRoutes from './pages/Auth'
+
+
+const Routes = () => {
+	return (
+		<Switch>
+			{/*<Route path={"/auth"} component={AuthRoutes} />*/}
+		</Switch>
+	)
+}
+
+export default Routes;
